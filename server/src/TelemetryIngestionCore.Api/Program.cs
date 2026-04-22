bc1f3b2090e6bc6ce6746a62ddb816bfc582e42b
Willa Charlotte <dev@wildflowersoup.dev>
2026-04-22 18:13:01 +0200

    feat(server): initial api and tests projects scaffolding

---
+++ server/src/TelemetryIngestionCore.Api/Program.cs
@@ -0,0 +1,16 @@
+namespace TelemetryIngestionCore.Api
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!").WithName("HelloWorld");
+
+            app.Run();
+        }
+    }
+}
---
+++ server/src/TelemetryIngestionCore.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TelemetryIngestionCore.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
