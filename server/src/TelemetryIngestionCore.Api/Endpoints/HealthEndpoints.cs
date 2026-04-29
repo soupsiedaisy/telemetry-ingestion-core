5de9e0e717ab460a3ed11acc506913b7bca39cdd
Willa Charlotte <dev@wildflowersoup.dev>
2026-04-29 15:50:36 +0200

    feat(server): add withname and produces to MapHealthEndpoints

--- server/src/TelemetryIngestionCore.Api/Endpoints/HealthEndpoints.cs
+++ server/src/TelemetryIngestionCore.Api/Endpoints/HealthEndpoints.cs
@@ -6,12 +6,19 @@ public static class HealthEndpoints
 {
     public static IEndpointRouteBuilder MapHealthEndpoints(this IEndpointRouteBuilder builder)
     {
-        builder.MapGet("/health/live", () => Results.Ok());
-        builder.MapGet(
-            "/health/ready",
-            (IHealthService healthService) =>
-                healthService.CheckDb() ? Results.Ok() : Results.Problem(statusCode: 503)
-        );
+        builder
+            .MapGet("/health/live", () => Results.Ok())
+            .WithName("HealthLive")
+            .Produces(statusCode: 200);
+        builder
+            .MapGet(
+                "/health/ready",
+                (IHealthService healthService) =>
+                    healthService.CheckDb() ? Results.Ok() : Results.Problem(statusCode: 503)
+            )
+            .WithName("HealthReady")
+            .Produces(200)
+            .Produces(503);
 
         return builder;
     }
